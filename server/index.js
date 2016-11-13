7aff39e439bf2563192ac6ea3b50da92849a0706
Carl Bai <carlbai@gmail.com>
2016-11-13 17:34:48 -0600

    Replaced Angular frontend with React

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static(__dirname + "/../client"));
+app.use(express.static(__dirname + "/../client/build"));
 
 app.get('/hi', function (req, res) {
   res.send('Hello World!')
 })
 
-app.use("*", express.static(__dirname + "/../client"));
+app.use("*", express.static(__dirname + "/../client/build"));
 
 app.listen(8080, function () {
   console.log(__dirname + "/../client");
