d66cacbae19c560a79854224c749bf63801be55a
Carl Bai <carlbai@gmail.com>
2016-11-29 22:38:52 -0600

    Got routing to work

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
-// import App from './App';
+import App from './App';
 import Home from './Home';
 import Watch from './Watch';
 import './index.css';
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.render(
   <Router history={browserHistory}>
     {/* add the routes here */}
-    <Route path="/" component={Home}>
+    <Route path="/" component={App}>
       <IndexRoute component={Home}/>
 
       <Route path="/watch/:videoID" component={Watch}/>
